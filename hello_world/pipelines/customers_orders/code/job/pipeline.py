2938443fae1e66b6cb1b8dbc888befad1a67877b
scott@prophecy.io <scott@prophecy.io>
2022-12-06 06:09:35 +0000

    ❇️ New     - Job        - Customers_Job
    🔁 Modified
        - Pipeline
           - customers_orders
           - report_top_customers

--- hello_world/pipelines/customers_orders/code/job/pipeline.py
+++ hello_world/pipelines/customers_orders/code/job/pipeline.py
@@ -8,10 +8,15 @@ from job.graph import *
 
 def pipeline(spark: SparkSession) -> None:
     df_Orders = Orders(spark)
+    df_Orders = collectMetrics(spark, df_Orders, "graph", "Source_28006", "65082")
     df_Customers = Customers(spark)
+    df_Customers = collectMetrics(spark, df_Customers, "graph", "Source_51275", "83296")
     df_By_CustomerId = By_CustomerId(spark, df_Orders, df_Customers)
+    df_By_CustomerId = collectMetrics(spark, df_By_CustomerId, "graph", "Join_98619", "31576")
     df_Cleanup = Cleanup(spark, df_By_CustomerId)
+    df_Cleanup = collectMetrics(spark, df_Cleanup, "graph", "Reformat_5054", "16363")
     df_Sum_Amounts = Sum_Amounts(spark, df_Cleanup)
+    df_Sum_Amounts = collectMetrics(spark, df_Sum_Amounts, "graph", "Aggregate_74495", "96556")
     Customer_Orders(spark, df_Sum_Amounts)
 
 def main():
@@ -23,6 +28,10 @@ def main():
                 .getOrCreate()\
                 .newSession()
     Utils.initializeFromArgs(spark, parse_args())
+    MetricsCollector.initializeMetrics(spark)
+    spark.conf.set("prophecy.collect.basic.stats", "true")
+    spark.conf.set("spark.sql.legacy.allowUntypedScalaUDF", "true")
+    spark.conf.set("spark.sql.optimizer.excludedRules", "org.apache.spark.sql.catalyst.optimizer.ColumnPruning")
     spark.conf.set("prophecy.metadata.pipeline.uri", "pipelines/customers_orders")
     
     MetricsCollector.start(
--- hello_world/pipelines/report_top_customers/code/job/pipeline.py
+++ hello_world/pipelines/report_top_customers/code/job/pipeline.py
@@ -3,23 +3,39 @@ from pyspark.sql.functions import *
 from pyspark.sql.types import *
 from job.config.ConfigStore import *
 from job.udfs.UDFs import *
+from prophecy.utils import *
 from job.graph import *
 
 def pipeline(spark: SparkSession) -> None:
     df_Customer_Orders = Customer_Orders(spark)
+    df_Customer_Orders = collectMetrics(spark, df_Customer_Orders, "graph", "Source_77973", "64023")
     df_By_Total_Amount = By_Total_Amount(spark, df_Customer_Orders)
+    df_By_Total_Amount = collectMetrics(spark, df_By_Total_Amount, "graph", "OrderBy_7525", "8295")
     df_Top_10 = Top_10(spark, df_By_Total_Amount)
+    df_Top_10 = collectMetrics(spark, df_Top_10, "graph", "Limit_20489", "21008")
     Report(spark, df_Top_10)
 
 def main():
-    spark = SparkSession.builder \
-        .config("spark.default.parallelism", "4") \
-        .config("spark.sql.legacy.allowUntypedScalaUDF", "true") \
-        .enableHiveSupport() \
-        .appName("Prophecy Pipeline") \
-        .getOrCreate()
+    spark = SparkSession.builder\
+                .config("spark.default.parallelism", "4")\
+                .config("spark.sql.legacy.allowUntypedScalaUDF", "true")\
+                .enableHiveSupport()\
+                .appName("Prophecy Pipeline")\
+                .getOrCreate()\
+                .newSession()
     Utils.initializeFromArgs(spark, parse_args())
+    MetricsCollector.initializeMetrics(spark)
+    spark.conf.set("prophecy.collect.basic.stats", "true")
+    spark.conf.set("spark.sql.legacy.allowUntypedScalaUDF", "true")
+    spark.conf.set("spark.sql.optimizer.excludedRules", "org.apache.spark.sql.catalyst.optimizer.ColumnPruning")
+    spark.conf.set("prophecy.metadata.pipeline.uri", "pipelines/report_top_customers")
+    
+    MetricsCollector.start(
+        spark = spark,
+        pipelineId = spark.conf.get("prophecy.project.id") + "/" + "pipelines/report_top_customers"
+    )
     pipeline(spark)
+    MetricsCollector.end(spark)
 
 if __name__ == "__main__":
     main()
